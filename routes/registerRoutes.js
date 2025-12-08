0e0c988096255ad432673092fc6c87b739900e84
Jackson Tullman <JacksonTullman1@Jacksons-MacBook-Pro.local>
2025-12-08 12:35:20 -0500

    create account button now displays errors properly, have to fix a couple reference bugs

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -19,6 +19,25 @@ router.route('/register').get(async (req, res) => {
         return res.status(403).render("error", {error:e})
         }
 })
+.post(async (req, res) => {
+    try {
+        //define registration terms
+        let firstName = req.body.firstName
+        let lastName = req.body.lastName
+        let email = req.body.email
+        let username = req.body.username
+        let password = req.body.password
+        let bio = req.body.bio
+        let picture = req.body.picture
+        //registering 
+        let newUser = await registerUsers(firstName,lastName,email,username,password,bio,picture)
+        //take back to home but now logged in 
+        return res.status('landingPage', {user:newUser})
+    }catch(e){
+        //error message
+        return res.status(403).render("error", {error:e})
+        }
+})
 
 
 export default router;
