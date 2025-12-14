ad2eb172ab7ac1bc377baf8adb4267da570135a0
Jacob Barbulescu <jrbarbulescu@gmail.com>
2025-12-14 12:48:59 -0500

    Updated the seend function to give the fountains proper formatting

--- tasks/seed.js
+++ tasks/seed.js
@@ -12,11 +12,50 @@ export const seedFunc = async () => {
         console.log("Fountains already seeded!");
         return
     }
+
     //seeding database using fetch
     let response  = await fetch(URL)
-    let result =  await response.json()
+    let results =  await response.json()
+
+    let formattedResults = [];
+    for (let result of results) {
+        let formattedResult = {};
+
+        //Format the bouroughs
+        if (result["borough"] = "M") result["borough"] = "Manhattan";
+        else if (result["borough"] = "R") result["borough"] = "Staten Island";
+        else if (result["borough"] = "Q") result["borough"] = "Queens";
+        else if (result["borough"] = "B") result["borough"] = "Brooklyn";
+        else if (result["borough"] = "X") result["borough"] = "Bronx";
+
+        formattedResult["borough"] = result["borough"];
+
+        //Location
+        formattedResult["location"] = [result["the_geom"]["coordinates"][0], result["the_geom"]["coordinates"][1]];
+
+        //Park
+        formattedResult["park"] = result["propertyna"];
+
+        //Ratings
+        formattedResult["averageRatings"] = {
+            taste: 0,
+            location: 0,
+		    pressure: 0,
+		    cleanliness: 0,
+		    accesibility: 0,
+		    operational: true
+        };
+
+        //Numfavorites
+        formattedResult["numFavorites"] = 0;
+
+        formattedResult["reviews"] = [];
+
+        formattedResults.push(formattedResult);
+    }
+
     //inserting into all fountains into collection
-    await fountainCollection.insertMany(result)
+    await fountainCollection.insertMany(formattedResults);
     //say seeded
     console.log("Fountain Data Seeded!");
 }
